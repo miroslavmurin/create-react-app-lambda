9686fe1b13deb7fe944d4d68fc70378c837432ce
blue.mio@gmail.com <blue.mio@gmail.com>
2022-02-01 12:01:06 +0100

    first test commit

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ class LambdaDemo extends Component {
 
     return (
       <p>
+        <button onClick={this.handleClick("hello yellow marshmallow")}>Welcome mesasge</button>
         <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
         <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
         <br />
